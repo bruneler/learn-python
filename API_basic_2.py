62369cb37c01f1c479e6c053d5fb4c6237040415
bruneler <24660127+bruneler@users.noreply.github.com>
2023-02-11 23:22:38 +0000

    renamed script.py to introduction_to_python.py. Added new basic content like loops, functions and classes.

---
+++ API_basic_2.py
@@ -0,0 +1,41 @@
+from ibm_watson import SpeechToTextV1  # SpeechToText library from ibm.
+from ibm_watson import LanguageTranslatorV3
+# API Keys. Ein Weg für Zugang zur API.
+
+
+# Wir erstellen einen Sprach-zu-Text programm mithilfe von der
+# IBM speechtotext Cloud API. To Use the API you need a free IBM Cloud Accound.
+url_s2t = "https://stream.watsonplatform.net/speech-to-text/api"
+iam_apikey_s2t = "thisismyAPIKey"
+s2t = SpeechToTextV1(iam_apikey=iam_apikey_s2t, url=url_s2t)  # Dieses Objekt wird für die Kommunikatin mit dem Service verwendet.
+
+filename = "pathtomyWAVfile.wav"
+with open(filename, mode="rb") as wav:
+    response = s2t.recognize(audio=wav, content_type="audio/wav")  # This sends a get result, the Audiofile to the Cloudservice
+    # content_type ist das audioformat.
+response.result  # Als Antowort erhalten wir ein Dictionary results
+
+#  Die Antwort:
+# {"result":[{"alternatives":[{confidence": 0.91, "transcript": "this will be the translated text"}], "final": True}], "result_index":0}
+
+# Wir benötigen nur den Inhalt des Dictionarys mit dem Key "transcript"
+recognized_text = response.result["results"][0]["alternatives"][0]["transcripts"]
+
+# Wir haben nun den Text aus dem WAV file. Dieses können wir nun noch in eine andere Sprache übersetzen
+
+url_lt = "https://gateway.watsonplatform.net/language-translator/api"
+apikey_lt = "myAPIKey"
+version_lt = "2018-05-01"
+
+language_translator = LanguageTranslatorV3(iam_apikey=apikey_lt, url=url_lt, version=version_lt)  # kreeirt das Objekt
+# language_translator.list_identifiable_languages().get_result()  # Zeigt alle verfügbaren Sprachen an etwa so:
+# {"languages":[{"language":"af", "name":"Afrikaas"}, ......]}
+
+translation_response = language_translator.translate(text=recognized_text, model_id="en-es")  # übersetzt gesprochene ins spanische
+translation = translation_response.get_result()
+#  {"translation":[{"translation":"der übersetzte text"{],
+#  "word_count":4, "character_count":21}
+# wir nehmen uns nur den Text
+spanisch_translation = translation["translation"][0]["translation"]
+print(spanisch_translation)
+
--- data_with_pandas.py
+++ data_with_pandas.py
@@ -1,4 +1,43 @@
-import pandas # populäre Library für Datenanalyse
-# ermöglicht funktionen und Methodne wie read_csv(), Series(), DataFrame(), values etc.
+# populäre Library für Datenanalyse
+# mit as kann ich der import Bibliothek einen
+# anderen Namen geben
+import pandas as derpanda
+
+
+# ermöglicht funktionen und Methoden wie read_csv(), Series(), DataFrame(), values etc.
+
+#csv_path = "file1.csv"
+#dataframe = derpanda.read_csv(csv_path)
+#dataframe.head()  # zeigt die ersten 5 Zeilen des Frames an.
+
+# Excel
+#xlsx_path = "file1.xlsx"
+#df = derpanda.read_excel(xlsx_path)
+#df.head()
+
+# Ein Dataframe aus einem Dictionary
+songs = {"Album": ["Thriller", "Back in Black", "The Dark side of the Moon", "The Bodyguard", "Bat Out of Hell"], "Released": [1982, 1989, 1973, 1992, 1977], "Length": ["00:42:19", "00:42:11", "00:42:49", "00:57:44", "00:46:33"]}
+songs_frame = derpanda.DataFrame(songs) # typecast mit panda funktion zu einem Dataframe
+print(songs_frame)
+
+# Neues Frame mit einzelnen Spalten
+
+Spalte12 = songs_frame[["Album", "Released"]]
+print(Spalte12)
+
+songs_frame["Released"].unique()
+
+# Dataframe Bedingungen
+new_songs_frame = songs_frame[songs_frame["Released"] >= 1980]
+print(new_songs_frame)
+
+# Speichern in csv
+
+new_songs_frame.to_csv("songs_after_1980.csv")
+
+print(songs_frame.loc[0:2, "Album"])
+print(songs_frame.iloc[0:2, 0])  # mit iloc am schluss eine zahl mehr
+# von:bis(Zeile),Spalte
+
 
 
